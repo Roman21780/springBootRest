b21cdf92a91690c2c007db3015cececcd6c46e5c
Roman <Sapunovrm21780@gmail.com>
2025-06-02 22:47:23 +0500

    Устранены замечания

--- src/main/java/ru/netology/springbootrest/SpringBootRestApplication.java
+++ src/main/java/ru/netology/springbootrest/SpringBootRestApplication.java
@@ -11,9 +11,4 @@ public class SpringBootRestApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootRestApplication.class, args);
     }
-
-    @Bean
-    public UserRepository userRepository() {
-        return new UserRepository();
-    }
 }
--- src/main/java/ru/netology/springbootrest/repository/UserRepository.java
+++ src/main/java/ru/netology/springbootrest/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package ru.netology.springbootrest.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.netology.springbootrest.model.Authorities;
-
 import java.util.Arrays;
 import java.util.List;
 
+@Repository
 public class UserRepository {
     public List<Authorities> getUserAuthorities(String user, String password) {
         // Имитация базы данных пользователей
