cb4afd96eff356d2abba8de0535daf7c89c18d30
Roman <Sapunovrm21780@gmail.com>
2025-06-04 00:26:27 +0500

    proxy-nginx

---
+++ src/main/java/ru/netology/springbootrest/controller/SigninController.java
@@ -0,0 +1,13 @@
+//package ru.netology.springbootrest.controller;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class SigninController {
+//
+//    @GetMapping("/signin")
+//    public String signin() {
+//        return "Welcome to the Sign In Page!";
+//    }
+//}
